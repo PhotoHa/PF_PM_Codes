5b398ab0ffd13eacc48e22f938b2b960b6d2d39b
PhotoHa <95481479+PhotoHa@users.noreply.github.com>
2024-03-06 22:41:47 +0900

    Update markov switching model.py

--- markov switching model.py
+++ markov switching model.py
@@ -2,7 +2,6 @@
 """
 Created on Fri Nov 25 09:57:41 2022
 
-@author: 11149
 """
 
 import numpy as np
@@ -14,7 +13,7 @@ import statsmodels.api as sm
 
 
 
-df = pd.read_csv("T:\\index\\95_곽용하\\연구\\국면_기준 탐구\\변동성_markov switching model\\200_rt.csv", index_col = 0)
+df = pd.read_csv("200_rt.csv", index_col = 0)
 df.index = pd.to_datetime(df.index, format="%Y%m%d")
 # Fit the model
 mod_kns = sm.tsa.MarkovRegression(
